55e6208023aeaca6c4726c42ae8758390a7ec61f
Kshitij <kshitij.mittal01@gmail.com>
2014-02-27 16:10:07 +0530

    Indexing Implemented

--- hellot.py
+++ hellot.py
@@ -23,9 +23,21 @@ class DbInsert(tornado.web.RequestHandler):
 		new_id=row[0].Id
 		self.write(str(row[0].Id)+"<br>")
 		#self.write(json_data['name'])
-		for k,v in json_data.items():
-			db.execute("Insert into KeyValueTable Values(%s,%s,%s)",new_id,k,v )
-		
+		indexkeys=list()
+		noIndex=True
+		self.write("Select * from IndexTable Where Type="+type1)
+		for row in db.query("Select * from IndexTable Where Type=\""+type1+"\""):
+			indexkeys.append(str(row.Index))
+			noIndex=False
+		self.write(str(indexkeys))
+		self.write(str(noIndex))
+		if noIndex:
+			for k,v in json_data.items():
+				db.execute("Insert into KeyValueTable Values(%s,%s,%s)",new_id,k,v )
+		else:
+			for k,v in json_data.items():
+				if (indexkeys.count(k)!=0):
+					db.execute("Insert into KeyValueTable Values(%s,%s,%s)",new_id,k,v )
 class DbQuery(tornado.web.RequestHandler):
 	def post(self):
 		id1=self.get_argument('Id1')
@@ -59,11 +71,18 @@ class DbRemove (tornado.web.RequestHandler):
 		removeId1='('+removeId1+')'
 		db.execute("DELETE FROM Obj_Table WHERE Id IN "+removeId1)
 		db.execute("DELETE FROM KeyValueTable WHERE Id IN "+removeId1)
+class DbIndex (tornado.web.RequestHandler):
+	def post(self):
+		type1=self.get_argument('type1')
+		index1=self.get_argument('index1')
+		db.execute("Insert into IndexTable Values (\""+type1+"\",\""+index1+"\")")
+		self.write("Insert into IndexTable Values (\""+type1+"\",\""+index1+"\")")
+		#self.render('index.html')
 if __name__ == "__main__":
 	tornado.options.parse_command_line()
 	db = torndb.Connection("localhost", "tordata",user="root",password="ksh")
 	app=tornado.web.Application(
-		handlers=[(r"/",IndexHandler),(r"/insert",DbInsert),(r"/query",DbQuery),(r"/search",DbSearch),(r"/remove",DbRemove)],
+		handlers=[(r"/",IndexHandler),(r"/insert",DbInsert),(r"/query",DbQuery),(r"/search",DbSearch),(r"/remove",DbRemove),(r"/indexKey",DbIndex)],
 		template_path=os.path.join(os.path.dirname(__file__),"templates"),
 		debug=True
 		)
