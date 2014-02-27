2b3a2147b69425a4f15a2df731ebe64e7c0c6ff3
Kshitij <kshitij.mittal01@gmail.com>
2014-02-27 14:54:46 +0530

    server-end method -remove- with (multiple simultaneous deletion) added

--- hellot.py
+++ hellot.py
@@ -45,18 +45,25 @@ class DbSearch(tornado.web.RequestHandler):
 			self.write (str(row.Id)+"<br>")
 			a.append(str(row.Id))
 		a=tuple(a)
-		self.write(str(a))
-		self.write(parameter1)
-		self.write(pValue1)
-		self.write(gle1)
+		#self.write(str(a))
+		#self.write(parameter1)
+		#self.write(pValue1)
+		#self.write(gle1)
 		self.write("<br> Select * from Obj_Table where Id IN "+str(a)+" and `"+str(parameter1)+"`" +gle1+" \""+str(pValue1)+"\"<br>")
 		for row in db.query("Select * from Obj_Table where Id IN "+str(a)+" and `"+str(parameter1)+"`" +gle1+ "\""+str(pValue1)+"\"" ):
 			self.write ("{\"Id\":"+str(row.Id) +",\"Obj\":"+ str(row.J_Obj)+",\"Type\":\""+str(row.Type)+"\",\"Timestamp\":\""+str(row.Timestamp)+"\"}"+"<br>")
+class DbRemove (tornado.web.RequestHandler):
+	def post(self):
+		removeId1=self.get_argument('removeId1')
+		self.write(removeId1)
+		removeId1='('+removeId1+')'
+		db.execute("DELETE FROM Obj_Table WHERE Id IN "+removeId1)
+		db.execute("DELETE FROM KeyValueTable WHERE Id IN "+removeId1)
 if __name__ == "__main__":
 	tornado.options.parse_command_line()
 	db = torndb.Connection("localhost", "tordata",user="root",password="ksh")
 	app=tornado.web.Application(
-		handlers=[(r"/",IndexHandler),(r"/insert",DbInsert),(r"/query",DbQuery),(r"/search",DbSearch)],
+		handlers=[(r"/",IndexHandler),(r"/insert",DbInsert),(r"/query",DbQuery),(r"/search",DbSearch),(r"/remove",DbRemove)],
 		template_path=os.path.join(os.path.dirname(__file__),"templates"),
 		debug=True
 		)
